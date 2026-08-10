a76506115eead66b301b84d17a336ae7f1cfb443
artyfartyy <dfghnm34@gmail.com>
2026-08-10 14:02:31 +0200

    Create rindfleisch.py

---
+++ rindfleisch.py
@@ -0,0 +1,14 @@
+
+def rindfleisch():
+    ы = "rindfleisch"
+    whopper = True
+
+
+    while whopper == True:
+        bruh = input()
+        if bruh == "":
+            print (ы)
+        else:
+            whopper = False
+
+rindfleisch()
