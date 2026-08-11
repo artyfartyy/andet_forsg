d400ec09d5edd61f15b38888613f2707495a3f1a
artyfartyy <dfghnm34@gmail.com>
2026-08-11 18:19:21 +0200

    implemented leaderboard!!:)

--- rindfleisch.py
+++ rindfleisch.py
@@ -3,7 +3,7 @@ import random
 import threading as t
 
 
-def rindfleisch():
+def rindfleisch(): # the game itself + encouraging messages
 
     counter = int(0)
     whopper = True
@@ -39,15 +39,19 @@ def rindfleisch():
                         elif counter == 10:
                             print("10! Game over.")
                             end = time.time()
-                            length = end - start
+                            leng = end - start
+                            length = round(leng, 3)
                             print(f"""
                             It took you {length} seconds to write Rindfleisch 10 times.""")
 
                             hmm = input("Add score to leaderboard? (y/n) ")
-
-                            if hmm == "y":
-                                 addtoleaderboard()
-            
+                            hmm = hmm.lower()
+                            if hmm in ["y", "yes", "yurr", "ye", "ы", "rindfleisch"]:
+                                name = input("Name: ")
+                                addtoleaderboard(name, length)
+                            else:
+                                 pass
+                            
                             quit()
     
                         
@@ -63,40 +67,65 @@ def rindfleisch():
                         """)
 #    def loss():
 #        global burger
-#        burger = 69420
+#        burger = 69420   - here I tried to make the game stop itself after 60 seconds, will retry at another point
         
 def viewleaderboard():
-    hamburg = open("leaderboard.txt", "r+")
+    hamburg = open("leaderboard.txt", "r")
+    print("")
     print(hamburg.read())
+    hamburg.close()
+    
 
-def addtoleaderboard(name, score):
-   
-    hamburg = open("leaderboard.txt", "a")
+def addtoleaderboard(name, length):
+    with open("leaderboard.txt", "a") as hamburg: # apparently, this saves me of a "hamburg.close()"
+        hamburg.write(f"{name}, {length} \n")
+    
+
+    with open("leaderboard.txt", "r") as bamhurg:
+        lines = bamhurg.readlines()
+
+    lines.sort(key = lambda line: float(line.split()[1]))
+
+    with open("leaderboard.txt", "w") as ron:
+         ron.writelines(lines)
+         
+
+
+    print("Score added.")
+
+
+def resetleaderboard():
+    hamburg = open("leaderboard.txt", "w")
+    hamburg.close()
+    print("Ain't that a fact")
     
 
 
 
-
-
-
-å = input("""
+choosing = input("""
     RINDFLEISCH NIGHT FUNKIN 1.1
     PRESS 'ENTER' TO PLAY
     INPUT 'L' TO VIEW LEADERBOARD
+    INPUT 'E' TO EXIT
     """)
 
-æ = å.lower()
+æ = choosing.lower()
 
-if æ == "":
-    rindfleisch()
+match æ: # if/else is still more spiritually fulfilling imo, I just had to try this out
+    case "":
+        rindfleisch()
 
-elif æ == "l":
-    viewleaderboard()
-        
+    case "l":
+        viewleaderboard()
 
-                
-else:
-    print("Fuck you.")                
+    case "fuck this game and everyone playing it":
+        resetleaderboard()
+
+    case "e":
+        quit()
+            
+    case _:
+        print("Fuck you.")
     
 
 
